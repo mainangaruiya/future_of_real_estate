2095c0f8f17c1d6b63d26f9e8330b54526085fc4
Maina Ngaruiya <100405059+mainangaruiya@users.noreply.github.com>
2024-01-23 15:27:42 +0300

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
 while true; do
-    # Get the process ID (PID) of the running Python app
     PID=$(pgrep -f "python app.py")
 
     if [ -n "$PID" ]; then
         echo "Stopping Python app with PID: $PID"
-        # Stop the Python app gracefully (e.g., send a termination signal)
         kill -TERM $PID
-        sleep 6 # Wait for the process to stop gracefully
+        sleep 6
     fi
 
-    # Restart the Python app
     echo "Restarting Python app"
     python app.py &
 
-    # Sleep for 10 minutes before the next iteration
     sleep 600
 done
